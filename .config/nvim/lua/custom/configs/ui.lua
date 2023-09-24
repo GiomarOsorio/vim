7cc3b554a1ecd7bc7681fbc5440a354ee317fc06
Giomar Osorio <giosorio30@gmail.com>
2023-09-24 14:38:31 -0500

    more changes

--- .config/nvim/lua/custom/configs/ui.lua
+++ .config/nvim/lua/custom/configs/ui.lua
@@ -1,11 +1,11 @@
 local ui = {
   transparency = true,
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
 
     -- default/round/block/arrow (separators work only for "default" statusline theme;
     -- round and block will work for the minimal theme only)
-    separator_style = "default",
+    separator_style = "arrow",
     overriden_modules = nil,
   },
   theme = 'gruvbox',
